b9fac53a64d439b4801c55792dac1441bd6cff45
fannyibz <fannyibanez89@gmail.com>
2021-11-04 12:01:34 +0100

    fix conflict

--- fanny.rb
+++ fanny.rb
@@ -1,6 +1,18 @@
 # create a branch.
+# commit fann 3
+"hello"
 # Update this file.
 # Merge to master when your boss tells you to
 "salut"
 # matt commit 3
+
+# fannny commit 2
+# matt commit 3
+<<<<<<< HEAD
 "salut paul"
+=======
+
+
+Riri
+loulou
+>>>>>>> f9c74bb754667e953b1bdb306774d3fb5e991b70
--- paul.rb
+++ paul.rb
@@ -8,3 +8,5 @@
 #final commit
 
 #ozeifjcosjif
+
+fifi
